fd496353573ee685f98a5476344c84a1980aaf55
wataru <ahrw5452@gmail.com>
2014-11-04 22:50:37 +0900

    1104夜修正

--- app/src/main/java/ws/milldesign_kd/watchView.java
+++ app/src/main/java/ws/milldesign_kd/watchView.java
@@ -1,5 +1,6 @@
 package ws.milldesign_kd;
 
+import android.app.ActionBar;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
@@ -16,16 +17,27 @@ public class watchView extends FragmentActivity implements View.OnClickListener 
 
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mDrawerToggle;
+    private ActionBar actionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_watch_view);
 
+        /*DrawerLatoutの設定*/
         mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
         mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
         mDrawerLayout.setDrawerListener(mDrawerToggle);
 
+        /*ActionBarの設定*/
+        actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setDisplayOptions(0,ActionBar.DISPLAY_SHOW_TITLE);
+        actionBar.setDisplayUseLogoEnabled(false);
+        actionBar.hide();
+
+        /*Toggleボタンの設定*/
         mDrawerToggle = new ActionBarDrawerToggle(
                 this,
                 mDrawerLayout,
@@ -34,17 +46,19 @@ public class watchView extends FragmentActivity implements View.OnClickListener 
                 R.string.drawer_close
         ){
             @Override
-            public void onDrawerClosed(View view){}
+            public void onDrawerClosed(View view){
+            }
             @Override
-            public void onDrawerOpened(View drawView){}
+            public void onDrawerOpened(View drawView){
+            }
             @Override
             public void onDrawerStateChanged(int newState) {
                 Log.i("NavigationDrawerLog", "NavigationDrawerの状態→" + newState+"(表示済み、閉じ済み：0  ドラッグ中:1  ドラッグを放した後のアニメーション中：2)");
             }
         };
-        getActionBar().setDisplayHomeAsUpEnabled(true);
-        getActionBar().setHomeButtonEnabled(true);
 
+        /*NavigationDrawer内の各ボタンのClickListener設定*/
+        findViewById(R.id.all_view).setOnClickListener(this);
         findViewById(R.id.Menu).setOnClickListener(this);
         findViewById(R.id.Timer).setOnClickListener(this);
         findViewById(R.id.Alarm).setOnClickListener(this);
@@ -54,6 +68,7 @@ public class watchView extends FragmentActivity implements View.OnClickListener 
     @Override
     public boolean onOptionsItemSelected(MenuItem item){
         if(mDrawerToggle.onOptionsItemSelected(item)){
+            actionBar.hide();
             return true;
         }
         return super.onOptionsItemSelected(item);
@@ -69,17 +84,21 @@ public class watchView extends FragmentActivity implements View.OnClickListener 
         mDrawerToggle.onConfigurationChanged(newConfig);
     }
 
-    /*ドロワーのボタンの設定*/
     @Override
     public void onClick(View v){
-        if(v == findViewById(R.id.Menu)){
+        if(v == findViewById(R.id.all_view)){
+            actionBar.show();
+        }else if(v == findViewById(R.id.Menu)){
             Intent toMenu = new Intent(this,menuMode.class);
             startActivity(toMenu);
         }else if(v == findViewById(R.id.Timer)){
+            actionBar.show();
             new timerDialogFragment().show(getFragmentManager(), "timer");
         }else if(v == findViewById(R.id.Alarm)){
+            actionBar.show();
             new alarmDialogFragment().show(getFragmentManager(), "alarm");
         }else if(v == findViewById(R.id.StopWatch)){
+            actionBar.show();
             new stopWatchDialogFragment().show(getFragmentManager(), "stopWatch");
         }
         mDrawerLayout.closeDrawers();
