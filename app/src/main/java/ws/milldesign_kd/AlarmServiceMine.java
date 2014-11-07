023cbb0a0e013dfe322740ae1a4b156002e058e9
sudo <ahrw5452@gmail.com>
2014-11-07 17:24:19 +0900

    1107午後編集

--- app/src/main/java/ws/milldesign_kd/AlarmServiceMine.java
+++ app/src/main/java/ws/milldesign_kd/AlarmServiceMine.java
@@ -5,6 +5,15 @@ import android.content.Intent;
 import android.os.IBinder;
 import android.util.Log;
 
+/*
+* サービスってクラスは、バックグラウンドで動くプログラムを作るクラス
+* 他のActivityのライフサイクルとは別に動くクラスの意
+* サービス作成時にはonCreateが呼ばれる
+* スレッドを別に作るとログの出力先が変わるので、
+* onCreateのLogが消される可能際がある(要調査)
+*
+*
+* */
 public class AlarmServiceMine extends Service {
 
     @Override
@@ -12,26 +21,36 @@ public class AlarmServiceMine extends Service {
         return null;
     }
 
+    //まずここでサービスを作る
     @Override
     public void onCreate() {
-        Log.i("まいあらーむさーびすろぐ（おんくりえいとのなか）","時間だぜ！！");
+        try {
+            Thread.sleep(3000,0);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Log.i("まいあらーむさーびすろぐ（おんくりえいとのなか）", "時間だぜ！！");
+
         Thread thr = new Thread(null, mTask, "MyAlarmServiceThread");
         thr.start();
-        Log.i("まいあらーむさーびすろぐ（おんくりえいとのなか）", "スレッド開始");
     }
-
     /**
      * アラームサービス
      */
     Runnable mTask = new Runnable() {
         public void run() {
             // ここでアラーム通知する前の処理など...
+            Log.i("まいあらーむさーびすろぐ(らんのなか)", "①");
             Intent alarmBroadcast = new Intent();
-            alarmBroadcast.setAction("MyAlarmAction");//独自のメッセージを送信します
+
+            //独自のメッセージを送信します
+            Log.i("まいあらーむさーびすろぐ(らんのなか)", "通知画面起動メッセージを送る");
+            alarmBroadcast.setAction("AlarmActionMine");
             sendBroadcast(alarmBroadcast);
-            Log.i("まいあらーむさーびすろぐ(らんのなか)", "通知画面起動メッセージを送った");
-            AlarmServiceMine.this.stopSelf();//サービスを止める
+
+            //サービスはいつ止める？
             Log.i("まいあらーむさーびすろぐ(らんのなか)", "サービス停止");
+            AlarmServiceMine.this.stopSelf();//サービスを止める
         }
     };
 }
